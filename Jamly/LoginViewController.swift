948e29c67b9b0b3c0431f06c6d8907cc9006193c
monitamitra <mitramonita@gmail.com>
2025-10-14 10:51:08 -0500

    setting up UI for login and signup screens

---
+++ Jamly/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Jamly
+//
+//  Created by Mitra, Monita on 10/14/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Jamly/SignupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignupViewController.swift
+//  Jamly
+//
+//  Created by Mitra, Monita on 10/14/25.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
