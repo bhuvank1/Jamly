9eb976738872e86d34740d63af23557c8c76925d
fareedah <fajisegiri@gmail.com>
2025-10-14 10:18:23 -0500

    added settings page options

---
+++ Jamly/SettingsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsViewController.swift
+//  Jamly
+//
+//  Created by Ajisegiri, Fareedah I on 10/14/25.
+//
+
+import UIKit
+
+public let settingsOptions = ["Account", "Enable Push Notifications", "Night Mode", "About", "Log out"]
+
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var settingsTableView: UITableView!
+    
+    let textCellIdentifier = "SettingsTextCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingsTableView.dataSource = self
+        settingsTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = settingsTableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = settingsOptions[indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell 
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
