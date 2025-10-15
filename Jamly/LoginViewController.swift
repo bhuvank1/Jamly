646a319f7cb373e87d1e9d8f82add34da562b16b
Mitra <mitramonita@gmail.com>
2025-10-15 12:42:37 -0500

    implemented basic login functionalityand added logo asset

--- Jamly/LoginViewController.swift
+++ Jamly/LoginViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        emailField.placeholder = "Enter your email"
+        passwordField.placeholder = "Enter your password"
+        passwordField.isSecureTextEntry = true
+        
+        // watches for a change in login status
+        Auth.auth().addStateDidChangeListener() {
+            (auth, user) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginToAppSegue", sender: nil)
+                self.emailField.text = ""
+                self.passwordField.text = ""
+            }
+        }
     }
     
+   
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!) {
+            (authResult, error) in
+            if let error = error as NSError? {
+                self.makePopup(popupTitle: "Error", popupMessage: error.localizedDescription)
+            }
+        }
+    }
+    
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "wantToCreateAccountSegue", sender: nil)
+    }
+    
+    func makePopup(popupTitle:String, popupMessage:String) {
+            
+            let controller = UIAlertController(
+                title: popupTitle,
+                message: popupMessage,
+                preferredStyle: .alert)
+            
+            controller.addAction(UIAlertAction(title: "OK", style: .default))
+            present(controller,animated:true)
+        }
 
 }
--- Jamly/SignupViewController.swift
+++ Jamly/SignupViewController.swift
@@ -6,13 +6,46 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailField.placeholder = "Enter your email"
+        passwordField.placeholder = "Enter your password"
+        passwordField.isSecureTextEntry = true
 
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) {
+            (authResult, error) in
+            if let error = error as NSError? {
+                self.makePopup(popupTitle: "Error", popupMessage: error.localizedDescription)
+            }
+        }
+        
+    }
+    
+    @IBAction func signInButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "wantToLoginSegue", sender: nil)
+    }
+    
+    func makePopup(popupTitle:String, popupMessage:String) {
+            
+            let controller = UIAlertController(
+                title: popupTitle,
+                message: popupMessage,
+                preferredStyle: .alert)
+            
+            controller.addAction(UIAlertAction(title: "OK", style: .default))
+            present(controller,animated:true)
+        }
+    
 }
