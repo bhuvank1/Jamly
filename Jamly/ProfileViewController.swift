50e0c1e73d8eb97599b799c04f6dfd4d757c1168
Rohan Pant <rohanpant2005@gmail.com>
2025-10-17 13:00:39 -0500

    initial profile view screen

---
+++ Jamly/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  Jamly
+//
+//  Created by Rohan Pant on 10/17/25.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var followersCountButton: UIButton!
+    @IBOutlet weak var followingCountButton: UIButton!
+    @IBOutlet weak var usernameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
