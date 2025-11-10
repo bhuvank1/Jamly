cbc5cd8ea12ae464013087e21ad749e6147a1087
Mitra <mitramonita@gmail.com>
2025-11-10 12:48:51 -0600

    made changes to post thumbnail in profile view controller

--- Jamly/PostDetailViewController.swift
+++ Jamly/PostDetailViewController.swift
@@ -11,9 +11,12 @@ class PostDetailViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
     var contentView: UIContentView!
-    var postImageView: UIImageView!
     @IBOutlet weak var captionTextView: UITextView!
     
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var songNameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var likesCount: UIButton!
     @IBOutlet weak var commentCount: UIButton!
     @IBOutlet weak var ratingLabel: UILabel!
@@ -27,6 +30,19 @@ class PostDetailViewController: UIViewController, UIScrollViewDelegate {
         ratingLabel.text = String("\(tempRating)/5")
         commentCount.setTitle(String(post?.comments.count ?? 0), for: .normal)
         likesCount.setTitle(String(post?.likes.count ?? 0), for: .normal)
+        usernameLabel.text = post?.displayName
+        songNameLabel.text = post?.trackObject.name
+        artistNameLabel.text = post?.trackObject.artists
+        
+        // image
+        if let albumArtURL = post?.trackObject.albumArt, let url = URL(string: albumArtURL) {
+            URLSession.shared.dataTask(with: url) { data, _, _ in
+                guard let data = data, let img = UIImage(data: data) else { return }
+                DispatchQueue.main.async {
+                    self.postImageView.image = img
+                }
+            }.resume()
+        }
         
     }
     
